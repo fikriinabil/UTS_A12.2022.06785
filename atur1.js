7eb9ed667f15098f9ce7289579539aca4fb6afa2
fikriinabil <112202206785@mhs.dinus.ac.id>
2025-11-29 22:01:24 +0700

    Add files via upload

---
+++ atur1.js
@@ -0,0 +1,8 @@
+function showTime() {
+    const now = new Date();
+    const time = now.toLocaleTimeString();
+    document.getElementById("clock").textContent = " | " + time;
+  }
+  setInterval(showTime, 1000);
+  showTime();
+  
